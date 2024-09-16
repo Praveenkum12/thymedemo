f4ce9d8e6b24f2cc2034a3ac5ea38b26c26f0b4e
PRAVEENKUMAR K <praveenkum1225@gmail.com>
2024-09-17 01:12:48 +0530

    first commit

--- src/main/java/com/jimmy/thymeleafdemo/Student.java
+++ src/main/java/com/jimmy/thymeleafdemo/Student.java
@@ -1,8 +1,37 @@
 package com.jimmy.thymeleafdemo;
 
+import java.util.List;
+
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
+    private String favouriteLanguage;
+    private List<String> os;
+
+    public List<String> getOs() {
+        return os;
+    }
+
+    public void setOs(List<String> os) {
+        this.os = os;
+    }
+
+    public String getFavouriteLanguage() {
+        return favouriteLanguage;
+    }
+
+    public void setFavouriteLanguage(String favouriteLanguage) {
+        this.favouriteLanguage = favouriteLanguage;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
     public Student() {
     }
--- src/main/java/com/jimmy/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/jimmy/thymeleafdemo/controller/StudentController.java
@@ -1,24 +1,33 @@
 package com.jimmy.thymeleafdemo.controller;
 
 import com.jimmy.thymeleafdemo.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
 
+    @Value("${countries}")
+    List<String> countries;
+
     @RequestMapping("/form")
     public String showForm(Model theModel) {
+        theModel.addAttribute("countries", countries);
         theModel.addAttribute("student", new Student());
+        theModel.addAttribute("demo", "Hello guys");
         return "student-form";
     }
 
     @PostMapping("/confirmPage")
     public String showConfirmationPage(@ModelAttribute("student") Student stud) {
         System.out.println(stud.getFirstName() + " " + stud.getLastName());
+//        System.out.println("///////////// " + (String) model.getAttribute("demo"));
         return "confirm-page";
     }
 }
